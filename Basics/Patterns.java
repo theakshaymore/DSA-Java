ec2d57fe7f22d6f4e2842ff2250d4a63d5dd4d5f
Akshay More <moreakshay725@gmail.com>
2024-07-18 22:02:37 +0530

    changes

---
+++ Basics/Patterns.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class Patterns {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println("*");
+            }
+
+            System.out.println("\n");
+        }
+    }
+}
