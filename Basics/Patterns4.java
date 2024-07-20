6ba488f64f7a31718844bdd9df541e8d62afd4f8
Akshay More <moreakshay725@gmail.com>
2024-07-20 14:04:02 +0530

    Pattern4

---
+++ Basics/Patterns4.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class Patterns4 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
