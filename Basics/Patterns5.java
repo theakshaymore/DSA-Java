666b83810d44e3dfa5cafa36fea9bca759557402
Akshay More <moreakshay725@gmail.com>
2024-07-20 15:31:59 +0530

    Pattern5

---
+++ Basics/Patterns5.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Patterns5 {
+    public static void main(String[] args) {
+
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
