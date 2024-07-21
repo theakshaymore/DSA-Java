068ebb421f499de97a4964f1923d740a15d89a79
Akshay More <moreakshay725@gmail.com>
2024-07-21 17:00:15 +0530

    arrylist methods

--- Collectionn/Listt/ArrayListt/Base.java
+++ Collectionn/Listt/ArrayListt/Base.java
@@ -1,13 +1,13 @@
 package Collectionn.Listt.ArrayListt;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class Base {
 
     public static void main(String[] args) {
 
-        Collection<Integer> nums = new ArrayList<Integer>();
+        List<Integer> nums = new ArrayList<Integer>();
 
         nums.add(2);
         nums.add(4);
@@ -16,6 +16,9 @@ public class Base {
         nums.add(5);
 
         // System.out.println(nums);
+        System.out.println(nums.get(3));
+        System.out.println(nums.size());
+        System.out.println(nums.indexOf(7));
 
         for (int n : nums) {
             System.out.println(n);
