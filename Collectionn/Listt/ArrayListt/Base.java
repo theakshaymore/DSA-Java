ad1346b0370e6aa8002412463c61117cf1924bdf
Akshay More <moreakshay725@gmail.com>
2024-07-21 16:52:54 +0530

    ArryList basics

--- Collectionn/Listt/ArrayListt/Base.java
+++ Collectionn/Listt/ArrayListt/Base.java
@@ -1,8 +1,29 @@
 package Collectionn.Listt.ArrayListt;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Base {
 
     public static void main(String[] args) {
+
+        Collection<Integer> nums = new ArrayList<Integer>();
+
+        nums.add(2);
+        nums.add(4);
+        nums.add(7);
+        nums.add(9);
+        nums.add(5);
+
+        // System.out.println(nums);
+
+        for (int n : nums) {
+            System.out.println(n);
+        }
+
+        // for (Object n : nums) {
+        // System.out.println((Integer) n);
+        // }
 
     }
 }
