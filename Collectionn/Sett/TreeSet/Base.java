4e36d33735d6aa1f6c20a29e00f78870e67b86e0
Akshay More <moreakshay725@gmail.com>
2024-07-21 18:07:26 +0530

    Treeset with Iterator

--- Collectionn/Sett/TreeSet/Base.java
+++ Collectionn/Sett/TreeSet/Base.java
@@ -1,5 +1,6 @@
 package Collectionn.Sett.TreeSet;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,13 +10,20 @@ public class Base {
         Set<Integer> nums = new TreeSet<Integer>();
 
         nums.add(33);
-        nums.add(12);
         nums.add(89);
+        nums.add(12);
         nums.add(35);
         nums.add(35);
 
         for (int n : nums) {
             System.out.println(n);
         }
+
+        // with Iterator
+        Iterator<Integer> values = nums.iterator();
+
+        while (values.hasNext())
+            System.out.println(values.next());
+
     }
 }
