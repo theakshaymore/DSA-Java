89282ccbfc9b71bf014243abb13a6818183b52fd
Akshay More <moreakshay725@gmail.com>
2024-07-21 17:48:35 +0530

    HashSet basics

--- Collectionn/Sett/HashSet/Base.java
+++ Collectionn/Sett/HashSet/Base.java
@@ -1,5 +1,24 @@
 package Collectionn.Sett.HashSet;
 
-public class Base {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Base {
+    public static void main(String[] args) {
+
+        Set<Integer> nums = new HashSet<Integer>();
+
+        nums.add(33);
+        nums.add(12);
+        nums.add(45);
+        nums.add(89);
+        nums.add(35);
+        nums.add(35);
+
+        for (int n : nums) {
+            System.out.println(n);
+        }
+
+        //
+    }
 }
