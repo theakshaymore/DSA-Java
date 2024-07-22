1b01c0f612b6fb96b5a371a7df27f419d7aad85d
Akshay More <moreakshay725@gmail.com>
2024-07-22 20:32:27 +0530

    comparator students

---
+++ Collectionn/Sorting/SortingStudents.java
@@ -0,0 +1,50 @@
+package Collectionn.Sorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Student {
+
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+
+}
+
+public class SortingStudents {
+    public static void main(String[] args) {
+
+        Comparator<Student> comparator = new Comparator<Student>() {
+            public int compare(Student i, Student j) {
+                if (i.age > j.age)
+                    return 1;
+                else
+                    return -1;
+
+            }
+        };
+
+        List<Student> student = new ArrayList<>();
+        student.add(new Student(23, "akshay"));
+        student.add(new Student(21, "rohit"));
+        student.add(new Student(24, "atharva"));
+        student.add(new Student(22, "nikhil"));
+
+        Collections.sort(student, comparator);
+        // System.out.println(student);
+
+        for (Student s : student)
+            System.out.println(s);
+
+    }
+}
