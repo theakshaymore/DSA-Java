01ea8ef6fb48e698bb5245b8e857a2ef5e8459b3
Akshay More <moreakshay725@gmail.com>
2024-07-22 20:15:13 +0530

    Comparator

--- Collectionn/Sorting/Sorting.java
+++ Collectionn/Sorting/Sorting.java
@@ -2,18 +2,29 @@ package Collectionn.Sorting;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Sorting {
     public static void main(String[] args) {
 
-        List<Integer> nums = new ArrayList<>();
-        nums.add(5);
-        nums.add(2);
-        nums.add(3);
-        nums.add(9);
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            public int compare(Integer i, Integer j) {
+                if (i % 10 > j % 10)
+                    return 1;
+                else
+                    return -1;
 
-        Collections.sort(nums);
+            }
+        };
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(55);
+        nums.add(29);
+        nums.add(34);
+        nums.add(97);
+
+        Collections.sort(nums, comparator);
         System.out.println(nums);
 
         //
