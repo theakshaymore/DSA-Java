0918c646cb4aff4dea478d21b440d45950bcdf71
Akshay More <moreakshay725@gmail.com>
2024-07-23 22:07:33 +0530

    base StreamsAPI

--- Collectionn/Streams/Base.java
+++ Collectionn/Streams/Base.java
@@ -1,9 +1,14 @@
 package Collectionn.Streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Base {
     public static void main(String[] args) {
-        
 
-        System.out.println("Hello");
+        List<Integer> nums = Arrays.asList(2, 8, 3, 5, 6);
+
+        System.out.println(nums);
+
     }
 }
