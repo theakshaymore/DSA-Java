c7a0aaf20dd5bce85ffa30f36153fb18d150de43
Akshay More <moreakshay725@gmail.com>
2024-07-23 22:15:34 +0530

    streams

--- Collectionn/Streams/Base.java
+++ Collectionn/Streams/Base.java
@@ -2,13 +2,16 @@ package Collectionn.Streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Base {
     public static void main(String[] args) {
 
         List<Integer> nums = Arrays.asList(2, 8, 3, 5, 6);
 
-        nums.forEach(n -> System.out.println(n * 2));
+        Stream<Integer> s1 = nums.stream();
+
+        s1.forEach(n -> System.out.println(n * 2));
 
         System.out.println(nums);
 
