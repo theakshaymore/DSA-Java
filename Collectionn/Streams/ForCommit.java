a55caec6aa29db9e5ee21af0e0e318d1e0239a35
Akshay More <moreakshay725@gmail.com>
2024-07-27 23:25:19 +0530

    comment undo5

--- Collectionn/Streams/ForCommit.java
+++ Collectionn/Streams/ForCommit.java
@@ -7,7 +7,7 @@ public class ForCommit {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
+        // System.out.println("hello");
         // System.out.println("hello");
         // System.out.println("hello");
         // System.out.println("hello");
