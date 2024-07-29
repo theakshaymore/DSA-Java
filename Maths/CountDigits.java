a3879dd6ab0e8153327d29f315397497be225324
Akshay More <moreakshay725@gmail.com>
2024-07-29 22:44:02 +0530

    count digits2

--- Maths/CountDigits.java
+++ Maths/CountDigits.java
@@ -4,13 +4,15 @@ public class CountDigits {
     public static void main(String[] args) {
         int num = 8732;
 
-        int count = 0, remainder = 0;
+        int count = 0;
 
-        while (num > 0) {
-            remainder = num % 10;
-            count++;
-            num = num / 10;
-        }
+        // while (num > 0) {
+        // int remainder = num % 10;
+        // count++;
+        // num = num / 10;
+        // }
+
+        count = (int) (Math.log10(num) + 1);
 
         System.out.println(count);
     }
