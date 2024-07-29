b1e6908b6a9b216ec8873b533dd53f63bf1adbf8
Akshay More <moreakshay725@gmail.com>
2024-07-29 23:05:57 +0530

    #3Palindrome

--- Maths/Palindrome.java
+++ Maths/Palindrome.java
@@ -2,16 +2,17 @@ package Maths;
 
 public class Palindrome {
     public static void main(String[] args) {
-        int num = 11;
+        int num = 1211;
         int reverseNum = 0;
 
+        int originalNum = num;
         while (num > 0) {
             int remainder = num % 10;
             num = num / 10;
             reverseNum = (reverseNum * 10) + remainder;
         }
 
-        if (reverseNum == num)
+        if (reverseNum == originalNum)
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
