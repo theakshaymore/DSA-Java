0a5c9d1a640af02bab00001ecc33fd7da345c839
Akshay More <moreakshay725@gmail.com>
2024-07-30 22:13:34 +0530

    #7leetcode

---
+++ Leetcode/Easy/ReverseNumber7.java
@@ -0,0 +1,19 @@
+package Leetcode.Easy;
+
+public class ReverseNumber7 {
+
+    // class Solution {
+    // public int reverse(int x) {
+    // int reverseNum = 0;
+
+    // while(x != 0)
+    // {
+    // int remainder = x % 10;
+    // x = x / 10;
+    // reverseNum = (reverseNum * 10) + remainder;
+    // }
+
+    // return reverseNum;
+    // }
+    // }
+}
