a1a53e9ff9ecbf876ae873b91a10ab226b952f1d
Akshay More <moreakshay725@gmail.com>
2024-08-03 22:42:27 +0530

    Forcommit

--- Collectionn/Streams/ForCommit.java
+++ Collectionn/Streams/ForCommit.java
@@ -3,7 +3,7 @@ package Collectionn.Streams;
 public class ForCommit {
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        // System.out.println("hello");
         // System.out.println("hello");
         // System.out.println("hello");
         // System.out.println("hello");
