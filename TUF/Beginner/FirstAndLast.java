e610d3114dd83de2cdda4a772eb525828f0dcd7e
Akshay More <moreakshay725@gmail.com>
2024-08-31 15:03:13 +0530

    elements

---
+++ TUF/Beginner/FirstAndLast.java
@@ -0,0 +1,8 @@
+public class FirstAndLast {
+    public static void main(String[] args) {
+
+        int[] nums = { 2, 5, 7, 3, 4 };
+
+        System.out.print(nums[0] + nums.length - 1);
+    }
+}
